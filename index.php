4cf2258d6bbcaf01de42f1f71f225a6768b7d677
Ryan Maskell <ryan@savorweb.com>
2014-06-20 16:09:11 -0700

    masonry

--- index.php
+++ index.php
@@ -42,26 +42,39 @@
 		</li>
 	</ul>
 	<h2>Heading 2</h2>
-	<img src="http://www.placehold.it/500x150" alt="">
+	<img class="right" src="http://www.placehold.it/500x300" alt="">
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, omnis, dolore, quos, numquam facilis voluptatum consectetur minima alias molestiae libero quibusdam rem error adipisci dolorem repellat accusamus dignissimos doloribus et!</p>
-	<img class="full-width" src="http://www.placehold.it/1000x300" alt="">
+	<img src="http://www.placehold.it/1000x300" alt="">
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, tempora, similique pariatur quo nostrum quia nam quaerat impedit optio commodi iste delectus nihil hic minus dolorum possimus sunt maxime dicta.</p>
 	<h3>Heading 3</h3>
 	<blockquote>
 		<p>The only thing worse than being blind, is having sight, and no vision.</p>
 		<i>Anonymous</i>
 	</blockquote>
+	<img class="left" src="http://www.placehold.it/400x300" alt="">
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, a non hic nam repellat dolorem voluptatum vero nihil quas beatae consequuntur eligendi et ipsum recusandae vitae magnam laborum laboriosam amet.</p>
 	<h4>Heading 4</h4>
-	<img class="left" src="http://www.placehold.it/400x300" alt="">
+	
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quae iure eius enim praesentium quia doloremque. Modi, molestias, nisi, cumque pariatur sint quaerat ullam recusandae reprehenderit eos dolores perferendis quidem.</p>
 	<ul>
 		<li>Things</li>
 		<li>Things</li>
 		<li>Things</li>
-		<li>Things</li>
-		<li>Things</li>
 	</ul>
+	
 </div>
+
+<div id="gallery" class="row editable-gallery">
+	<a href="http://www.placehold.it/500x600" class="fancybox"><img src="http://www.placehold.it/500x600" alt=""></a>
+	<a href="http://www.placehold.it/300x300" class="fancybox"><img src="http://www.placehold.it/300x300" alt=""></a>
+	<a href="http://www.placehold.it/800x300" class="fancybox"><img src="http://www.placehold.it/800x300" alt=""></a>
+	<a href="http://www.placehold.it/200x500" class="fancybox"><img src="http://www.placehold.it/200x300" alt=""></a>
+	<a href="http://www.placehold.it/500x300" class="fancybox"><img src="http://www.placehold.it/500x300" alt=""></a>
+	<a href="http://www.placehold.it/800x300" class="fancybox"><img src="http://www.placehold.it/800x300" alt=""></a>
+	<a href="http://www.placehold.it/200x500" class="fancybox"><img src="http://www.placehold.it/200x300" alt=""></a>
+	<a href="http://www.placehold.it/500x300" class="fancybox"><img src="http://www.placehold.it/500x300" alt=""></a>
+	<a href="http://www.placehold.it/300x300" class="fancybox"><img src="http://www.placehold.it/300x300" alt=""></a>
+</div>
+
 
 <?php include("_/inc/foot.php"); ?>
